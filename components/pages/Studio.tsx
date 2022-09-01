0b494ab9224df5d3fbfc4a454a92885d57422275
Jean <chakib3500@gmail.com>
2022-09-01 22:30:19 +0200

    studio init

---
+++ components/pages/Studio.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function Studio() {
+  
+    const Studio = styled.div`
+        height: 200vh;
+        background: white;
+    `
+
+    return (
+    <Studio>
+
+    </Studio>
+  )
+}
